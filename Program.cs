0b3585f2d3cf9d8ef463342a1f9d4375b4152441
Steve Hansen <steve@hansenconsultancy.be>
2023-10-17 11:56:25 +0200

    feat: Added Bump

--- Program.cs
+++ Program.cs
@@ -1,13 +1,67 @@
 ﻿// 'Dev' tool can be used to do some development tasks based on the current directory.
 
 using System.Diagnostics;
+using System.Text.RegularExpressions;
+using Dev;
 
 var path = Environment.CurrentDirectory;
+
+var command = "launch";
+if (args.Length > 0)
+{
+    command = args[0];
+
+    if (command == "help")
+    {
+        Console.WriteLine("Usage: dev [command]");
+        Console.WriteLine("Commands:");
+        Console.WriteLine("  launch (default) - Launches the current project in Visual Studio or Visual Studio Code.");
+        Console.WriteLine("  bump [major|minor|patch|revision] - Bumps the version of all projects in the current solution.");
+        Console.WriteLine("  help - Displays this help message.");
+        return;
+    }
+}
 
 // Check if we have a .sln file in the current directory. And if so, open it in Visual Studio.
 var slnFile = Directory.GetFiles(path, "*.sln").FirstOrDefault();
 if (slnFile != null)
 {
+    if (command == "bump")
+    {
+        var subCommand = "patch";
+        if (args.Length > 1)
+            subCommand = args[1];
+
+        // Will bump all versions inside all csproj files linked in the solution
+        var sln = File.ReadAllText(slnFile);
+        var matches = ProjectRegex().Matches(sln);
+        foreach (Match match in matches)
+        {
+            var name = match.Groups["name"].Value;
+            var projectPath = match.Groups["path"].Value;
+            var csproj = File.ReadAllText(projectPath);
+            var versionMatch = VersionRegex().Match(csproj);
+            if (versionMatch.Success)
+            {
+                var version = versionMatch.Groups["version"].Value;
+                var semver = new SemVer(version);
+                // Bump based on subCommand (major, minor, patch or revision)
+                var newVersion = subCommand switch
+                {
+                    "major" => new(semver.Major + 1, 0, 0, 0, semver.Suffix, semver.BuildVariables),
+                    "minor" => new(semver.Major, semver.Minor + 1, 0, 0, semver.Suffix, semver.BuildVariables),
+                    "patch" => new(semver.Major, semver.Minor, semver.Build + 1, 0, semver.Suffix, semver.BuildVariables),
+                    _ => new SemVer(semver.Major, semver.Minor, semver.Build, semver.Fix + 1, semver.Suffix, semver.BuildVariables),
+                };
+                Console.WriteLine($"Bumping {name} from {version} to {newVersion}...");
+                csproj = VersionRegex().Replace(csproj, $"<Version>{newVersion}</Version>");
+                File.WriteAllText(projectPath, csproj);
+            }
+        }
+
+        return;
+    }
+
     Console.WriteLine($"Opening {slnFile} in Visual Studio...");
     Process.Start(new ProcessStartInfo(slnFile) { UseShellExecute = true });
     return;
@@ -26,3 +80,12 @@ if (csprojFile != null)
 
 // Nothing to do.
 Console.WriteLine("No .sln or .csproj file found in the current directory.");
+
+partial class Program
+{
+    [GeneratedRegex("Project\\(\"\\{.*\\}\"\\)\\s*=\\s*\"(?<name>.*)\",\\s*\"(?<path>.*)\",\\s*\"\\{.*\\}\"")]
+    private static partial Regex ProjectRegex();
+
+    [GeneratedRegex("<Version>(?<version>.*)</Version>")]
+    private static partial Regex VersionRegex();
+}
---
+++ SemVer.cs
@@ -0,0 +1,159 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Dev;
+
+public sealed partial class SemVer : IComparable<SemVer>, IEquatable<SemVer>
+{
+    public readonly int Major;
+    public readonly int? Minor, Build, Fix;
+    public readonly string? Suffix, BuildVariables;
+    public readonly bool IsAny;
+
+    public SemVer(int major, int? minor, int? build, int? fix, string? suffix, string? buildVariables)
+    {
+        Major = major;
+        Minor = minor;
+        Build = build;
+        Fix = fix;
+        Suffix = suffix;
+        BuildVariables = buildVariables;
+    }
+
+    public SemVer(string src)
+    {
+        if (string.IsNullOrEmpty(src) || src == "*")
+        {
+            IsAny = true;
+            return;
+        }
+        var m = VersionRegex().Match(src);
+        if (!m.Success)
+            throw new FormatException($"Unparsable version string: {src}");
+        Major = int.Parse(m.Groups["major"].Value);
+        if (m.Groups["minor"].Success)
+            Minor = int.Parse(m.Groups["minor"].Value);
+        if (m.Groups["build"].Success)
+            Build = int.Parse(m.Groups["build"].Value);
+        if (m.Groups["fix"].Success)
+            Fix = int.Parse(m.Groups["fix"].Value);
+        if (m.Groups["suffix"].Success)
+            Suffix = m.Groups["suffix"].Value;
+        if (m.Groups["buildvars"].Success)
+            BuildVariables = m.Groups["buildvars"].Value;
+    }
+
+    public bool Equals(SemVer? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Major == other.Major && Minor == other.Minor && Build == other.Build && Fix == other.Fix && string.Equals(Suffix, other.Suffix) && IsAny == other.IsAny;
+    }
+
+    public int CompareTo(SemVer? other)
+    {
+        if (ReferenceEquals(this, other)) return 0;
+        if (other is null) return 1;
+        var majorComparison = Major.CompareTo(other.Major);
+        if (majorComparison != 0) return majorComparison;
+        var minorComparison = Nullable.Compare(Minor, other.Minor);
+        if (minorComparison != 0) return minorComparison;
+        var buildComparison = Nullable.Compare(Build, other.Build);
+        if (buildComparison != 0) return buildComparison;
+        var fixComparison = Nullable.Compare(Fix, other.Fix);
+        if (fixComparison != 0) return fixComparison;
+        var suffixComparison = CompareSuffix(Suffix, other.Suffix);
+        return suffixComparison != 0 ? suffixComparison : IsAny.CompareTo(other.IsAny);
+    }
+
+    private static int CompareSuffix(string? a, string? b)
+    {
+        if (string.IsNullOrEmpty(a) && string.IsNullOrEmpty(b)) return 0;
+        if (string.IsNullOrEmpty(a)) return 1;
+        if (string.IsNullOrEmpty(b)) return -1;
+        var aparts = a.Split('.');
+        var bparts = b.Split('.');
+        for (int i = 0, l = Math.Max(aparts.Length, bparts.Length); i < l; i++)
+        {
+            if (aparts.Length <= i)
+                return -1;
+            if (bparts.Length <= i)
+                return 1;
+
+            bool ai = int.TryParse(aparts[i], out var aa),
+                bi = int.TryParse(bparts[i], out var bb);
+            if (ai && bi)
+                if (aa != bb)
+                    return aa - bb;
+                else
+                    continue;
+            if (ai) return -1;
+            if (bi) return 1;
+            var sc = string.Compare(aparts[i], bparts[i], StringComparison.Ordinal);
+            if (sc != 0)
+                return sc;
+        }
+        return 0;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        return obj.GetType() == GetType() && Equals((SemVer)obj);
+    }
+
+    public override int GetHashCode() => HashCode.Combine(Major, Minor, Build, Fix, Suffix, IsAny);
+
+    private sealed class SemVerEqualityComparer : IEqualityComparer<SemVer>
+    {
+        public bool Equals(SemVer? x, SemVer? y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (x is null) return false;
+            if (y is null) return false;
+            if (x.GetType() != y.GetType()) return false;
+            return x.Major == y.Major && x.Minor == y.Minor && x.Build == y.Build && x.Fix == y.Fix && string.Equals(x.Suffix, y.Suffix) && x.IsAny == y.IsAny;
+        }
+
+        public int GetHashCode(SemVer obj)
+        {
+            unchecked
+            {
+                var hashCode = obj.Major;
+                hashCode = hashCode * 397 ^ obj.Minor.GetHashCode();
+                hashCode = hashCode * 397 ^ obj.Build.GetHashCode();
+                hashCode = hashCode * 397 ^ obj.Fix.GetHashCode();
+                hashCode = hashCode * 397 ^ (obj.Suffix != null ? obj.Suffix.GetHashCode() : 0);
+                hashCode = hashCode * 397 ^ obj.IsAny.GetHashCode();
+                return hashCode;
+            }
+        }
+    }
+
+    public static IEqualityComparer<SemVer> SemVerComparer { get; } = new SemVerEqualityComparer();
+
+    public override string ToString()
+    {
+        if (IsAny)
+            return "*";
+        var rv = Major.ToString();
+        if (Minor.HasValue)
+        {
+            rv += "." + Minor;
+            if (Build.HasValue)
+            {
+                rv += "." + Build;
+                if (Fix.HasValue)
+                    rv += "." + Fix;
+            }
+        }
+        if (!string.IsNullOrEmpty(Suffix))
+            rv += "-" + Suffix;
+        if (!string.IsNullOrEmpty(BuildVariables))
+            rv += "+" + BuildVariables;
+        return rv;
+    }
+
+    [GeneratedRegex("^(?<major>[0-9]+)(\\.(?<minor>[0-9]+))?(\\.(?<build>[0-9]+))?(\\.(?<fix>[0-9]+))?(-(?<suffix>.*))?(\\+(?<buildvars>.*))?$", RegexOptions.IgnoreCase | RegexOptions.Compiled, "en-BE")]
+    private static partial Regex VersionRegex();
+}
