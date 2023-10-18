a53d7bca483a67ba97d7cc48bf09535ba68fada2
Steve Hansen <steve@hansenconsultancy.be>
2023-10-18 11:30:05 +0200

    feat: Handle bump and build on project as well

--- Program.cs
+++ Program.cs
@@ -11,13 +11,13 @@ if (args.Length > 0)
 {
     command = args[0];
 
-    if (command == "help")
+    if (command is "help")
     {
         Console.WriteLine("Usage: dev [command]");
         Console.WriteLine("Commands:");
         Console.WriteLine("  launch (default) - Launches the current solution in Visual Studio or project in Visual Studio Code.");
-        Console.WriteLine("  bump [major|minor|patch|revision] - Bumps the version of all projects in the current solution.");
-        Console.WriteLine("  build - Builds the current solution in Release mode.");
+        Console.WriteLine("  bump [major|minor|patch|revision] - Bumps the version of all projects in the current solution or the current project.");
+        Console.WriteLine("  build - Builds the current solution or project in Release mode.");
         Console.WriteLine("  help - Displays this help message.");
         return;
     }
@@ -27,47 +27,22 @@ if (args.Length > 0)
 var slnFile = Directory.GetFiles(path, "*.sln").FirstOrDefault();
 if (slnFile != null)
 {
-    if (command == "bump")
+    if (command is "bump")
     {
-        var subCommand = "patch";
-        if (args.Length > 1)
-            subCommand = args[1];
+        var subCommand = args.Length > 1 ? args[1] : "patch";
 
         // Will bump all versions inside all csproj files linked in the solution
         var sln = File.ReadAllText(slnFile);
         var matches = ProjectRegex().Matches(sln);
         foreach (Match match in matches)
-        {
-            var name = match.Groups["name"].Value;
-            var projectPath = match.Groups["path"].Value.Replace('\\', Path.DirectorySeparatorChar);
-            var csproj = File.ReadAllText(projectPath);
-            var versionMatch = VersionRegex().Match(csproj);
-            if (versionMatch.Success)
-            {
-                var version = versionMatch.Groups["version"].Value;
-                var semver = new SemVer(version);
-                // Bump based on subCommand (major, minor, patch or revision)
-                var newVersion = subCommand switch
-                {
-                    "major" => new(semver.Major + 1, 0, 0, semver.Fix is null ? semver.Fix : 0, semver.Suffix, semver.BuildVariables),
-                    "minor" => new(semver.Major, semver.Minor + 1, 0, semver.Fix is null ? semver.Fix : 0, semver.Suffix, semver.BuildVariables),
-                    "patch" => new(semver.Major, semver.Minor, semver.Build + 1, semver.Fix is null ? semver.Fix : 0, semver.Suffix, semver.BuildVariables),
-                    _ => new SemVer(semver.Major, semver.Minor, semver.Build, semver.Fix + 1, semver.Suffix, semver.BuildVariables),
-                };
-                Console.WriteLine($"Bumping {name} from {version} to {newVersion}...");
-                csproj = VersionRegex().Replace(csproj, $"<Version>{newVersion}</Version>");
-                File.WriteAllText(projectPath, csproj);
-            }
-        }
+            BumpProjectVersion(match.Groups["path"].Value.Replace('\\', Path.DirectorySeparatorChar), subCommand);
 
         return;
     }
 
     if (command is "build")
     {
-        // Use dotnet build to build the solution in Release mode
-        Console.WriteLine($"Building {slnFile} in Release mode...");
-        Process.Start("dotnet", $"build \"{slnFile}\" -c Release");
+        BuildSolutionOrProject(slnFile);
         return;
     }
 
@@ -82,6 +57,19 @@ if (slnFile != null)
 var csprojFile = Directory.GetFiles(path, "*.csproj").FirstOrDefault();
 if (csprojFile != null)
 {
+    if (command is "bump")
+    {
+        // Will bump the version inside the current csproj file
+        BumpProjectVersion(csprojFile, args.Length > 1 ? args[1] : "patch");
+        return;
+    }
+
+    if (command is "build")
+    {
+        BuildSolutionOrProject(csprojFile);
+        return;
+    }
+
     Console.WriteLine($"Opening {csprojFile} in Visual Studio Code...");
     Process.Start("code", csprojFile);
     return;
@@ -89,6 +77,38 @@ if (csprojFile != null)
 
 // Nothing to do.
 Console.WriteLine("No .sln or .csproj file found in the current directory.");
+
+static void BumpProjectVersion(string projectPath, string subCommand)
+{
+    var csproj = File.ReadAllText(projectPath);
+    var versionMatch = VersionRegex().Match(csproj);
+    if (versionMatch.Success)
+    {
+        var version = versionMatch.Groups["version"].Value;
+        var semver = new SemVer(version);
+        // Bump based on subCommand (major, minor, patch or revision)
+        var newVersion = subCommand switch
+        {
+            "major" => new(semver.Major + 1, 0, 0, semver.Fix is null ? semver.Fix : 0, semver.Suffix, semver.BuildVariables),
+            "minor" => new(semver.Major, semver.Minor + 1, 0, semver.Fix is null ? semver.Fix : 0, semver.Suffix, semver.BuildVariables),
+            "patch" => new(semver.Major, semver.Minor, semver.Build + 1, semver.Fix is null ? semver.Fix : 0, semver.Suffix, semver.BuildVariables),
+            _ => new SemVer(semver.Major, semver.Minor, semver.Build, semver.Fix + 1, semver.Suffix, semver.BuildVariables),
+        };
+
+        Console.WriteLine($"Bumping {Path.GetFileNameWithoutExtension(projectPath)} from {version} to {newVersion}...");
+        csproj = VersionRegex().Replace(csproj, $"<Version>{newVersion}</Version>");
+        File.WriteAllText(projectPath, csproj);
+    }
+    else
+        Console.WriteLine($"No version found in {Path.GetFileNameWithoutExtension(projectPath)}.");
+}
+
+static void BuildSolutionOrProject(string path)
+{
+    // Use dotnet build to build the solution or project in Release mode
+    Console.WriteLine($"Building {Path.GetFileName(path)} in Release mode...");
+    Process.Start("dotnet", $"build \"{path}\" -c Release");
+}
 
 partial class Program
 {
